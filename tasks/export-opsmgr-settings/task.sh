15e4c0a48fefefc5274359ee8b9560b0eb80cab7
Brandon Hudlemeyer <bhudlemeyer@gmail.com>
2017-07-03 10:59:18 -0700

    modify opsman-timeout during upgrade

--- tasks/export-opsmgr-settings/task.sh
+++ tasks/export-opsmgr-settings/task.sh
@@ -25,7 +25,7 @@ function main() {
      --skip-ssl-validation \
      --username "${OPSMAN_USERNAME}" \
      --password "${OPSMAN_PASSWORD}" \
-     --request-timeout 6000 \
+     --request-timeout "${OPSMAN_EXPORT_TIMEOUT}  \
      export-installation \
      --output-file "${cwd}/opsmgr-settings/${OPSMAN_SETTINGS_FILENAME}"
 }

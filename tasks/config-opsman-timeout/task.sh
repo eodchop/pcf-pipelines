d7236ea6d64642a534c679e6247347a8b0fafa9d
Brandon Hudlemeyer <bhudlemeyer@gmail.com>
2017-07-13 09:56:48 -0700

    fix bash script

--- tasks/config-opsman-timeout/task.sh
+++ tasks/config-opsman-timeout/task.sh
@@ -4,14 +4,14 @@ apt-get install ssh expect -y
 
 /usr/bin/expect << EOD
 set timeout 50
-spawn ssh -o "StrictHostKeyChecking=no" ubuntu@$env(OPSMANIP)
+spawn ssh -o "StrictHostKeyChecking=no" ubuntu@(OPSMANIP)
 
 expect "*assword:"
-send "$env(OPSPW)\r";
+send "(OPSPW)\r";
 expect "*$\*"
 send "sudo su -\r";
 expect "*ubuntu:"
-send "$env(OPSPW)\r";
+send "(OPSPW)\r";
 expect "*~#"
 send "sed -i 's/1800/6000/g' /home/tempest-web/tempest/web/config/thin.production.yml\r";
 expect "*~#"

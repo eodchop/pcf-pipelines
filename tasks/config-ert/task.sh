3dfe64aa45d45b8c621f2cc4591c008eb5fe1ec0
Brandon Hudlemeyer <bhudlemeyer@gmail.com>
2017-07-24 10:14:45 -0700

    fix conflicts

--- tasks/config-ert/task.sh
+++ tasks/config-ert/task.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-<<<<<<< HEAD
 set -eu
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $script_dir/configure_product.sh
 configure_product
-=======
 
 chmod +x tool-om/om-linux
 
